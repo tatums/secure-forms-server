b5896c2d3f3da5e6a30f85629bf17ee95bbb070c
Tatum <tatum@ashlandstudios.com>
2015-11-15 23:28:58 -0600

    implement user show

--- app.js
+++ app.js
@@ -45,6 +45,14 @@ router.post('/messages', function(req, res) {
 
 // User Routes
 
+router.get('/users/:id', function(req, res) {
+  User.findById(req.params.id, function(err, user) {
+    if (err)
+      res.send(err);
+    res.json(user);
+  });
+});
+
 router.get('/users', function(req, res) {
   User.find(function(err, users) {
     if (err)
@@ -77,11 +85,9 @@ router.put('/users/:id', function(req, res) {
 });
 
 router.post('/users', function(req, res) {
-
   var user = new User();
   user.firstName = req.body.firstName;
   user.lastName = req.body.lastName;
-
   // save the user and check for errors
   user.save(function(err) {
     if (err)
